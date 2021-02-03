4a744e3acf2b0f0d7a63f1dc87797821df9967a6
Diego Molina <molinacrespo@gmail.com>
2021-02-03 13:15:53 +0100

    Remaining tests to be implemented

---
+++ tests/Unit/Http/Models/AdvertiserTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit\Http\Models;
+
+use PHPUnit\Framework\TestCase;
+
+class AdvertiserTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
